6fad973d6e046453bd0176b4acf58e515c7c47c5
shenjianwen <1317250867@qq.com>
2019-05-26 23:42:02 +0800

    city滑动

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
+  <div class="cityMain">
      <div class="header">
        <span class="iconfont icon-city">&#xe624;</span>
         城市选择
      </div>
-     <div class="main">
-      <div class="city-main">
-        <h3 class="hot-title">热门城市</h3>
-        <div class="city-item">
-          <div class="city-item-is border-topright" v-for="item in hotCityList" :key="item.id">{{item.name}}</div>
-        </div>
-      </div>
-      <div class="letter-main">
-        <div class="city-main" v-for="(i, k) of letterList" :key="k">
-          <h3 class="hot-title">{{k}}</h3>
+     <div class="main wapperMain" ref="wapper">
+      <div>
+        <div class="city-main">
+          <h3 class="hot-title">热门城市</h3>
           <div class="city-item">
-            <div class="city-item-is border-topright" v-for="letter of i" :key="letter.id">{{letter.name}}</div>
+            <div class="city-item-is border-topright" v-for="item in hotCityList" :key="item.id">{{item.name}}</div>
+          </div>
+        </div>
+        <div class="letter-main">
+          <div class="city-main" v-for="(i, k) of letterList" :key="k">
+            <h3 class="hot-title">{{k}}</h3>
+            <div class="city-item">
+              <div class="city-item-is border-topright" v-for="letter of i" :key="letter.id">{{letter.name}}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -25,7 +27,7 @@
 
 <script>
 import axios from 'axios'
-import bSceoll from 'better-scroll'
+import Bsceoll from 'better-scroll'
 export default {
   name: '',
   created () {
@@ -41,40 +43,55 @@ export default {
       hotCityList: []
     }
   },
-  methods: {}
+  methods: {},
+  mounted () {
+
+    new Bsceoll(this.$refs.wapper)
+  }
 }
 </script>
 <style lang='less' scoped>
 @import '~@/assets/css/varibles.less';
-    .header{
-      background-color:@bgColor;
-      position: relative;
-      text-align: center;
-      color:#ffffff;
-      padding:.3rem;
-      font-size:.3rem;
-      .icon-city{
-        position: absolute;
-        left:.2rem;
+.wapperMain{
+      position: absolute;
+      top:.9rem;
+      bottom:0;
+      right:0;
+      left:0;
+      overflow: hidden;
+}
+    .cityMain{
+      .header{
+        background-color:@bgColor;
+        position: relative;
+        z-index:99;
+        text-align: center;
+        color:#ffffff;
+        padding:.3rem;
+        font-size:.3rem;
+        .icon-city{
+          position: absolute;
+          left:.2rem;
+        }
       }
-    }
-    .city-main{
-      width:100%;
-      color:#000000;
-      .hot-title{
-        font-size:.2rem;
-        padding:.2rem 0;
-        padding-left:.3rem;
-        background-color: #eeeeee;;
-      }
-      .city-item{
-        display:flex;
+      .city-main{
         width:100%;
-        flex-wrap:wrap;
-        .city-item-is{
-          text-align:center;
+        color:#000000;
+        .hot-title{
+          font-size:.2rem;
           padding:.2rem 0;
-          flex:0 0 33.33%;
+          padding-left:.3rem;
+          background-color: #eeeeee;;
+        }
+        .city-item{
+          display:flex;
+          width:100%;
+          flex-wrap:wrap;
+          .city-item-is{
+            text-align:center;
+            padding:.2rem 0;
+            flex:0 0 33.33%;
+          }
         }
       }
     }
