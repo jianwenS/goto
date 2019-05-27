5cd2bd305626edad2e46d2d9d413a85d0758b31c
shenjianwen <1317250867@qq.com>
2019-05-27 23:11:44 +0800

    city编写

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -4,30 +4,17 @@
        <span class="iconfont icon-city">&#xe624;</span>
         城市选择
      </div>
-     <div class="main wapperMain" ref="wapper">
-      <div>
-        <div class="city-main">
-          <h3 class="hot-title">热门城市</h3>
-          <div class="city-item">
-            <div class="city-item-is border-topright" v-for="item in hotCityList" :key="item.id">{{item.name}}</div>
-          </div>
-        </div>
-        <div class="letter-main">
-          <div class="city-main" v-for="(i, k) of letterList" :key="k">
-            <h3 class="hot-title">{{k}}</h3>
-            <div class="city-item">
-              <div class="city-item-is border-topright" v-for="letter of i" :key="letter.id">{{letter.name}}</div>
-            </div>
-          </div>
-        </div>
-      </div>
+     <div class="main">
+        <cityRight :letterList="letterList"></cityRight>
+        <cityList :letterList="letterList" :hotCityList="hotCityList"></cityList>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import Bsceoll from 'better-scroll'
+import cityList from '@/pages/city/compoents/cityList'
+import cityRight from '@/pages/city/compoents/cityRight'
 export default {
   name: '',
   created () {
@@ -44,22 +31,14 @@ export default {
     }
   },
   methods: {},
-  mounted () {
-
-    new Bsceoll(this.$refs.wapper)
+  components: {
+    cityList,
+    cityRight
   }
 }
 </script>
 <style lang='less' scoped>
 @import '~@/assets/css/varibles.less';
-.wapperMain{
-      position: absolute;
-      top:.9rem;
-      bottom:0;
-      right:0;
-      left:0;
-      overflow: hidden;
-}
     .cityMain{
       .header{
         background-color:@bgColor;
@@ -72,26 +51,6 @@ export default {
         .icon-city{
           position: absolute;
           left:.2rem;
-        }
-      }
-      .city-main{
-        width:100%;
-        color:#000000;
-        .hot-title{
-          font-size:.2rem;
-          padding:.2rem 0;
-          padding-left:.3rem;
-          background-color: #eeeeee;;
-        }
-        .city-item{
-          display:flex;
-          width:100%;
-          flex-wrap:wrap;
-          .city-item-is{
-            text-align:center;
-            padding:.2rem 0;
-            flex:0 0 33.33%;
-          }
         }
       }
     }
---
+++ src/pages/city/compoents/cityList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class=" wapperMain" ref="wapper">
+      <div>
+        <div class="city-main">
+          <h3 class="hot-title">热门城市</h3>
+          <div class="city-item">
+            <div class="city-item-is border-topright" v-for="item in hotCityList" :key="item.id">{{item.name}}</div>
+          </div>
+        </div>
+        <div class="letter-main">
+          <div class="city-main" v-for="(i, k) of letterList" :key="k">
+            <h3 class="hot-title">{{k}}</h3>
+            <div class="city-item">
+              <div class="city-item-is border-topright" v-for="letter of i" :key="letter.id">{{letter.name}}</div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bsceoll from 'better-scroll'
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  props: ['letterList', 'hotCityList'],
+  methods: {},
+  mounted () {
+    new Bsceoll(this.$refs.wapper)
+  }
+}
+</script>
+<style lang='less' scoped>
+    .wapperMain{
+        position: absolute;
+        top:.9rem;
+        bottom:0;
+        right:0;
+        left:0;
+        overflow: hidden;
+              .city-main{
+          width:100%;
+          color:#000000;
+          .hot-title{
+            font-size:.2rem;
+            padding:.2rem 0;
+            padding-left:.3rem;
+            background-color: #eeeeee;;
+          }
+          .city-item{
+            display:flex;
+            width:100%;
+            flex-wrap:wrap;
+            .city-item-is{
+              text-align:center;
+              padding:.2rem 0;
+              flex:0 0 33.33%;
+            }
+          }
+        }
+    }
+</style>
